111cc153f52bdac7eb32f234416ba86203e0714e
Cary Dreelan <cary@active-ideas.com.au>
2015-12-17 00:03:10 +1030

    Use simulated UserAgent when crawling as some sites don't like 'curl' user agent

--- shell/classes/FeedParser.php
+++ shell/classes/FeedParser.php
@@ -196,6 +196,8 @@ abstract class FeedParser
     curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($curlSession, CURLOPT_FOLLOWLOCATION, true);
     curl_setopt($curlSession, CURLOPT_MAXREDIRS, 2);
+    curl_setopt($curlSession, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)"); 
+
 
     $content = @curl_exec($curlSession);
     $effectiveUrl = @curl_getinfo($curlSession, CURLINFO_EFFECTIVE_URL);
